a11b3ec176c882c338d0f5d33e99a80f0c5e4527
Leonardo <44807664+LeonardoZauli@users.noreply.github.com>
2026-05-15 20:50:47 +0200

    Modifica visibilità TV8

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iptv1.app',
+  appName: 'IPTV1',
+  webDir: 'dist'
+};
+
+export default config;
